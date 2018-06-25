41c3fb8ebd8bb70fe1e5b59ccdd4054474ce989b
kongvungsovanreach <vungsovanreachkong@gmail.com>
2018-06-25 23:54:32 +0700

    Final Code Thank cher

---
+++ Downloads/article_management_system-master/src/main/java/com/kps/kvsvr/articlemanagement/ArticlemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.kps.kvsvr.articlemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlemanagementApplication.class, args);
+	}
+}
